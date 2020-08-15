17bbcd335b2709bd663630b9002263216c658542
Ilya Zhidkov <xkelxmc@yandex.ru>
2020-08-15 16:02:46 +0300

    add eslint; add prettier

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import express from 'express';
 
 const app = express();
 
-const port = 3012;
+const port = process.env.PORT || 3012;
 
 app.get('/ping', (req, res) => {
-  return res.json('pong');
+    return res.json('pong');
 });
 
 app.listen(port, () => {
-    console.log(`App started at port ${port}`)
-})
+    console.log(`App started at port ${port}`);
+});
