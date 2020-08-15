1bc8dbb9a2672fa409acdda4f090069cee853595
Ilya Zhidkov <xkelxmc@yandex.ru>
2020-08-15 15:41:46 +0300

    add ping:pong; add dotenv

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 const port = 3012;
 
+app.get('/ping', (req, res) => {
+  return res.json('pong');
+});
+
 app.listen(port, () => {
     console.log(`App started at port ${port}`)
 })
